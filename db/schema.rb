b5ec47c4251e40818414340654de4b459ea6c4d2
Marko Ivanek <markoivanek@gmail.com>
2013-04-08 18:16:40 +0200

    Finish first version of user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404120724) do
+ActiveRecord::Schema.define(:version => 20130408160931) do
 
   create_table "bookmarks", :force => true do |t|
     t.string   "title"
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(:version => 20130404120724) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
