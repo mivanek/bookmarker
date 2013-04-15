734383ef28a26a6e536faadd08c218aa8b347f77
Marko Ivanek <markoivanek@gmail.com>
2013-04-15 17:57:09 +0200

    Create model for folders

---
+++ app/controllers/folders_controller.rb
@@ -0,0 +1,19 @@
+class FoldersController < ApplicationController
+  def new
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
