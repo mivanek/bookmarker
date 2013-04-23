542f55cad5109ae8e38be5ace2f046c4b01f810d
Marko Ivanek <markoivanek@gmail.com>
2013-04-23 18:09:43 +0200

    Fix tests, improve old tests

---
+++ spec/requests/bookmarks_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Bookmarks" do
+  let(:user){ FactoryGirl.create(:user) }
+  let(:bookmark){ FactoryGirl.create(:bookmark, user_id: user.id) }
+  let!(:folder){ FactoryGirl.create(:folder, user_id: user.id) }
+  describe "destroy action" do
+    before do
+      sign_in user
+      visit bookmarks_path
+    end
+
+    it { page.should have_link('Delete', href: bookmark_path(Bookmark.first)) }
+    it "should be able do delete a bookmark" do
+      expect { page.click_link('Delete', href: bookmark_path(Bookmark.first)) }.
+        to change(Bookmark, :count).by(-1)
+    end
+  end
+end
