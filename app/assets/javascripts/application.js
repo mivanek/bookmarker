a68cb28e36f847d1a266f65481fd227d8908a769
Marko Ivanek <markoivanek@gmail.com>
2013-05-01 18:26:15 +0200

    Add arrow to folders, make sure that bookmarks switch between folders/no_folder correctly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,6 @@
 //= require_tree .
 
 $(window).load(function () {
-  var dropped = false;
-  var draggable_sibling;
 
   // let folder be droppable
   $('.folder, .foldered-bookmark').droppable({
@@ -26,10 +24,43 @@ $(window).load(function () {
     hoverClass: 'hovered-folder',
     drop: function(event, ui) {
       if($(ui.draggable).hasClass("bookmark")) {
-        $(ui.draggable).removeClass("bookmark").addClass("foldered-bookmark");
         var id = $(ui.draggable).attr('id').replace(/bookmark-/, '');
+        if ($(this).hasClass('folder')) {
+          var folder_id = $(this).attr('id').replace(/folder-/, '');
+        }
+        if ($(this).hasClass('foldered-bookmark')) {
+          $(this).removeClass('foldered-bookmark ui-droppable');
+          var folder_id = $(this).attr('class').replace(/folder-id-/, '');
+          $(this).addClass('foldered-bookmark ui-droppable');
+        }
+
+        $(ui.draggable).removeClass("bookmark").
+          addClass("foldered-bookmark").addClass("folder-id-"+folder_id);
         $(ui.draggable).attr('id', "foldered-bookmark-"+id);
-        $('#bookmark_table tbody').sortable("refresh");
+      }
+      else if ($(ui.draggable).hasClass("foldered-bookmark")) {
+
+        if ($(this).hasClass('folder')) {
+          var folder_id = $(this).attr('id').replace(/folder-/, '');
+
+          $(ui.draggable).removeClass('foldered-bookmark ui-droppable');
+
+          var old_id = $(ui.draggable).attr('class');
+
+          old_id = old_id.replace('folder-id-', '');
+
+          $(ui.draggable).removeClass('folder-id-'+old_id).
+            addClass('ui-droppable foldered-bookmark folder-id-'+folder_id);
+        }
+
+        else if ($(this).hasClass('foldered-bookmark')){
+          $(this).removeClass('foldered-bookmark ui-droppable');
+          var new_class = $(this).attr('class');
+
+          $(ui.draggable).removeAttr('class');
+          $(ui.draggable).addClass('foldered-bookmark ui-droppable '+new_class);
+          $(this).addClass('foldered-bookmark ui-droppable');
+        }
       }
     }
   });
@@ -39,8 +70,9 @@ $(window).load(function () {
     hoverClass: 'hovered-folder',
     drop: function(event, ui) {
       if ($(ui.draggable).hasClass("foldered-bookmark")) {
-        $(ui.draggable).removeClass("foldered-bookmark").addClass("bookmark");
         var id = $(ui.draggable).attr('id').replace(/foldered-bookmark-/, '');
+
+        $(ui.draggable).removeAttr('class').addClass("bookmark ui-draggable");
         $(ui.draggable).attr('id', 'bookmark-'+id);
       }
     }
@@ -49,6 +81,7 @@ $(window).load(function () {
   $('#bookmarks_table tbody').sortable({
     axis: "y",
     opacity: 0.6,
+    distance: 20,
     stop: function(event, ui){
       var element_ids = $("#bookmarks_table tbody").last().sortable('serialize');
       var pobj = {element_ids: element_ids};
@@ -58,14 +91,17 @@ $(window).load(function () {
   });
 
 
-  $('.folder > tr:first').accordion({
-    collapsible: true
+  $('tr.folder').click(function () {
+    var id = $(this).attr('id').replace(/folder-/, '');
+
+    $('.folder-id-'+id).slideToggle('fast');
+
+    if($('.arrow-'+id).attr('src') == '/assets/arrow-down.png') {
+      $('.arrow-'+id).attr('src', '/assets/arrow-right.png');
+    }
+
+    else if($('.arrow-'+id).attr('src') == '/assets/arrow-right.png') {
+      $('.arrow-'+id).attr('src', '/assets/arrow-down.png');
+    }
   });
 });
-
-
-    //accept: function(d) {
-      //if(d.hasClass("foldered-bookmark")||d.hasClass("bookmark")) {
-        //return true;
-      //}
-    //}
